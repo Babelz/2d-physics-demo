ad450d0598c3f092df0f24c1c61d43b6f4b35202
Jani Vattula <janivattula@outlook.com>
2018-11-17 20:22:54 +0200

    Physics World with Sweep & Prune algorithm

--- 2d-physics-demo/2d-physics-demo/include/physics_world.hpp
+++ 2d-physics-demo/2d-physics-demo/include/physics_world.hpp
@@ -4,6 +4,12 @@
 #include "rigid_body.hpp"
 #include <vector>
 
+struct SweepPair
+{
+	float location;
+	fs::RigidBody* body;
+};
+
 class PhysicsWorld
 {
 public:
@@ -17,6 +23,11 @@ public:
 private:
 	float updateRate;
 	float stepTime;
-	std::vector<fs::RigidBody*> bodyArray;
 
+	std::vector<fs::RigidBody*> bodyArray;
+	std::vector<SweepPair> sweepPairArray;
+	std::vector<std::pair<fs::RigidBody*, fs::RigidBody*>> collisionPairs;
+	std::vector<fs::RigidBody*> activeBodies;
+
+	bool comparePairs(SweepPair* a, SweepPair* b);
 };
--- 2d-physics-demo/2d-physics-demo/src/physics_world.cpp
+++ 2d-physics-demo/2d-physics-demo/src/physics_world.cpp
@@ -1,5 +1,6 @@
 #include "physics_world.hpp"
 #include "shape.hpp"
+#include <algorithm>
 
 PhysicsWorld::PhysicsWorld(float stepsPerSecond)
 {
@@ -22,13 +23,13 @@ void PhysicsWorld::step(float delta)
 	if (stepTime >= (1.0f / updateRate))
 	{
 		// Apply forces and torques to objects
-		for (int i = 0; i < bodyArray.size(); i++)
+		for (unsigned int i = 0; i < bodyArray.size(); i++)
 		{
 			// Compute force and torque
 		}
 
 		// Update object locations, velocities and rotations
-		for (int i = 0; i < bodyArray.size(); i++)
+		for (unsigned int i = 0; i < bodyArray.size(); i++)
 		{
 			fs::RigidBody* updateBody = bodyArray[i];
 			// Calculate linear acceleration
@@ -55,11 +56,70 @@ void PhysicsWorld::step(float delta)
 		}
 
 		// Detect collisions
+		// Add all min and max x-values to an array with their bodies
+		unsigned int pairIndex = 0;
+		sweepPairArray.reserve(bodyArray.size() * 2);
+		for (unsigned int i = 0; i < bodyArray.size(); i++)
+		{
+			sweepPairArray[pairIndex].location = bodyArray[i]->aabb.min.x;
+			sweepPairArray[pairIndex].body = bodyArray[i];
+			pairIndex++;
 
+			sweepPairArray[pairIndex].location = bodyArray[i]->aabb.max.x;
+			sweepPairArray[pairIndex].body = bodyArray[i];
+			pairIndex++;
+		}
+		
+		// Sort the pair list by the x-value
+		std::sort(sweepPairArray.begin(), sweepPairArray.end(), comparePairs);
+		
+		// Go through sorted array
+		unsigned int sweepIndex = 0;
+		while (sweepIndex < sweepPairArray.size())
+		{
+			// Add first (a) to active list
+			activeBodies[sweepIndex] = sweepPairArray[sweepIndex].body;
 
+			// If a.maxx is met before b.minx, remove from active list, add b to active list
+			if (sweepIndex + 1 < sweepPairArray.size())
+			{
+				if (activeBodies[sweepIndex] == sweepPairArray[sweepIndex + 1].body)
+				{
+					activeBodies.erase(std::begin(activeBodies) + sweepIndex);
+					sweepIndex += 2;
+					continue;
+				}
+				else
+				{
+					activeBodies[sweepIndex + 1] = sweepPairArray[sweepIndex + 1].body;
+				}
+			}
+
+			//If there are active bodies, add pair (a,b) to check collisions list
+			if (activeBodies.size() >= 2)
+			{
+				for (unsigned int i = 0; i < activeBodies.size(); i++)
+				{
+					for (unsigned int j = (i + 1); j < activeBodies.size(); j++)
+					{
+						std::pair<fs::RigidBody*, fs::RigidBody*> collisionPair = std::make_pair(activeBodies[i], activeBodies[j]);
+						collisionPairs.push_back(collisionPair);
+					}
+				}
+			}
+			// Continue until list is iterated through
+		}
+	
+		// Remove duplicates from collision pairs
 		// Solve collisions
 
 		stepTime = 0.0f;
 	}
 
+}
+
+// Compare pair x-values for sorting.
+bool PhysicsWorld::comparePairs(SweepPair* a, SweepPair* b)
+{
+	return (a->location < b->location);
 }
