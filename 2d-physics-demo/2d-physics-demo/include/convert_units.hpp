392f1671f890a9dd4d4c829e8d5ad2535b1d02b5
babelz <niko.halikka@gmail.com>
2018-11-17 14:03:39 +0200

    add doc

--- 2d-physics-demo/2d-physics-demo/include/convert_units.hpp
+++ 2d-physics-demo/2d-physics-demo/include/convert_units.hpp
@@ -1,14 +1,27 @@
 #pragma once
 
+
 namespace fs {
+	// Contains utilities that help converting values between 
+	// screen and world spaces.
 
 	class Vector2;
 
+	// Sets the ratio for converting units from screen units to world
+	// units and back.
 	void setScreenUnitToWorldUnitRatio(float screenUnitsPerWorldUnit);
 
+	// Converts given world units to screen units.
 	float toScreenUnits(float worldUnits);
+
+	// Converts given vector value from world units
+	// to screen units.
 	Vector2 toScreenUnits(Vector2& worldUnits);
 
+	// Converts given screen units to world units.
 	float toWorldUnits(float screenUnits);
+
+	// Converts given vector value from screen units
+	// to world units.
 	Vector2 toWorldUnits(Vector2& screenUnits);
 }
